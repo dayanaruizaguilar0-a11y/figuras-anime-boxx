d003ca0993e10e2b757d7fcb3c733c37c41ea09b
dayanaruizaguilar0-a11y <dayanaruizaguilar0@gmail.com>
2026-09-15 17:39:29 +1200

    subir proyecto

---
+++ script.js
@@ -0,0 +1,552 @@
+const producto = [
+    {
+        nombre: "Figura Coleccionable Goku en Moto",
+        precio:" 85",
+        imagenes: [
+                "IMG/gojan 85.jpe ",
+                 "IMG/gohanbici2.jpe ",
+                "IMG/gohanbici3.jpe"
+],
+        descripcion: "Material: PVC/ABS resistente y de alta densidad. mide aprox. 12cm de alto"
+    },
+
+    {
+        nombre: "figura coleccionable maestro roshi en moto",
+        precio:" 85",
+        imagenes: [
+         "IMG/roshi.jpe",
+         "IMG/roshi2.jpe",
+         "IMG/roshi3.jpe"
+],
+        descripcion: "Material: PVC/ABS resistente y de alta densidad. mide aprox. 12cm de alto"
+    },
+    {
+        nombre: "figura coleccionable de goku con gohan ",
+        precio:" 120",
+        imagenes: [
+         "IMG/goku 120.jpe",
+         "IMG/gokugohan2.jpe",
+         "IMG/goku 120.jpe"
+],
+        descripcion: "lMaterial: PVC/ABS resistente y de alta densidad. mide aprox. 17 cm de alto"
+    },
+    {
+        nombre: "figura coleccionable Kid Buu (Dragon Ball Z) ",
+        precio:" 90",
+        imagenes: [
+         "IMG/malo rosa.jpe",
+         "IMG/rosa malo2.jpe",
+         "IMG/rosamalo3.jpe"
+],
+        descripcion: "Material: PVC/ABS resistente y de alta densidad."
+    },
+    {
+        nombre: "figura coleccionable Majin Buu Gordo con Bee (Dragon Ball Z) ",
+        precio:" 90",
+        imagenes: [
+         "IMG/roa perrito.jpe",
+         "IMG/rosaperrito2.jpe",
+         "IMG/rositaperrito3.jpe"
+],
+        descripcion: "Material: PVC/ABS resistente y de alta densidad."
+    },
+    {
+        nombre: "Diorama Escultórico Goku Evolución / Super Saiyan con Dragón Shenron",
+        precio:" 360",
+        imagenes: [
+         "IMG/360 goku.jpe",
+         "IMG/goku3202.jpe",
+         "IMG/goku3333.jpe"
+],
+        descripcion: " Material habitual: PVC rígido reforzado, ABS y partes de resina plástica translúcida .mide aprox 35cm"
+    },
+    {
+        nombre: "Figura Coleccionable Milk (Chi-Chi) Niña en Nube Kinton  Dragon Ball",
+        precio:" 70",
+        imagenes: [
+         "IMG/milck.jpe",
+         "IMG/milk2.jpe",
+         "IMG/milk3.jpe"
+],
+        descripcion: "Material: PVC/ABS resistente y de alta densidad."
+    },
+    {
+        nombre: "Figura Crossover Pikachu x Majin Vegeta Super Saiyan – Edición Fusión",
+        precio:" 40",
+        imagenes: [
+         "IMG/vegueta pikachu.jpe",
+         "IMG/pikavege2.jpe",
+         "IMG/vegueta pikachu.jpe"
+],
+        descripcion: "Material: PVC y ABS, mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Crossover Pikachu x Naruto Modo Sabio con Pergamino – Edición Shinobi",
+        precio:" 110",
+        imagenes: [
+         "IMG/picachunaruto.jpe",
+         "IMG/pikanaru2.jpe",
+         "IMG/pikanaru3.jpe"
+],
+        descripcion: "mide aprox 12cm"
+    },{
+        nombre: "Figura Crossover Pikachu x Kakashi Hatake – Naruto",
+        precio:" 70",
+        imagenes: [
+         "IMG/pikahashi.jpe",
+         "IMG/pikakaka2.jpe",
+         "IMG/pikahashi3.jpe"
+],
+        descripcion: "Material: PVC / ABS rígido.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Crossover Pikachu x Meliodas (Pecado del Dragón) – Nanatsu no Taizai",
+        precio:" 50",
+        imagenes: [
+         "IMG/pikananatsu.jpe",
+         "IMG/chibi melo2.jpe",
+         "IMG/chibimelo3.jpe"
+],
+        descripcion: "Material: PVC y ABS rígido.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Crossover Pikachu x Itachi Uchiha ANBU – Naruto Shippuden",
+        precio:" 70",
+        imagenes: [
+         "IMG/pikaninja.jpe",
+         "IMG/pikasask2.jpe",
+         "IMG/pikasaske3.jpe"
+],
+        descripcion: "Material: PVC / ABS.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Crossover Pikasakura Fusión Ninja – Naruto Shippuden",
+        precio:" 120",
+        imagenes: [
+         "IMG/pikasacura.jpe",
+         "IMG/pikasacura2.jpe",
+         "IMG/pikasacura3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS de alta densidad.mide aprox 12cm"
+    },
+    {
+        nombre: "Figura Crossover Pikasuke / Pika-Itachi Fusión Ninja – Edición Akatsuki con Luna Sharingan",
+        precio:" 120",
+        imagenes: [
+         "IMG/pikasaske.jpe",
+         "IMG/pika saske 2.jpe",
+         "IMG/pikasaske.jpe"
+],
+        descripcion: "Material: PVC / ABS rígido con acabado mate y detalles en pintura acrílica.Tamaño: Aprox. 12–13 cm."
+    },
+    {
+        nombre: "Figura Crossover Pikachu x Sasuke Uchiha (Ataque Chidori) – Naruto Shippuden",
+        precio:" 120",
+        imagenes: [
+         "IMG/pikasavol.jpe",
+         "IMG/pikasavol2.jpe",
+         "IMG/pikasavol3.jpe"
+],
+        descripcion: "Material: PVC rígido y resina translúcida para los efectos de rayos.mide aprox 13cm"
+    },
+    {
+        nombre: "Figura Coleccionable Fusión Épica – Pikachu Sabio Jiraiya y Bulbasaur",
+        precio:" 120",
+        imagenes: [
+         "IMG/pikamaestr.jpe",
+         "IMG/pikamaestr 2.jpe",
+         "IMG/pikamaestr.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido.mide aprox 11cm"
+    },
+     {
+        nombre: "Figura Crossover Pikachu x Zenitsu: El Arte del Trueno – Demon Slayer",
+        precio:" 90",
+        imagenes: [
+         "IMG/pikazeni.jpe",
+         "IMG/pikazeni2.jpe",
+         "IMG/pika senit3.jpe"
+],
+        descripcion: "Material: PVC / ABS con detalles translúcidos en los rayos.mide aprox 11cm"
+    },
+     {
+        nombre: "Figura Coleccionable Kanao Tsuyuri (Versión Sentada / Edge) – Demon Slayer",
+        precio:" 50",
+        imagenes: [
+         "IMG/demon kanao.jpe",
+         "IMG/demon kanao 2.jpe",
+         "IMG/demon kanao 3.jpe"
+],
+        descripcion: "Material: PVC y ABS moldeado.mide aprox 14cm"
+    },
+    {
+        nombre: "Diorama Zenitsu Agatsuma – Primera Postura: Destello del Relámpago y luz led",
+        precio:" 260",
+        imagenes: [
+         "IMG/zetled.jpe",
+         "IMG/zed led2.jpe",
+         "IMG/zed led3.jpe"
+],
+        descripcion: "Material: PVC / ABS rígido con efectos en resina translúcida.mide aprox 14cm"
+    },
+     {
+        nombre: "Figura Coleccionable Mitsuri Kanroji (Versión Sentada / Relax) – Kimetsu no Yaiba",
+        precio:" 50",
+        imagenes: [
+         "IMG/demonmtsury.jpe",
+         "IMG/demonmtsury 2.jpe",
+         "IMG/demonmtsury 3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS.mide aprox 14cm"
+    },
+     {
+        nombre: "Figura Coleccionable Obanai Iguro (Versión Sentada / Relax) – Kimetsu no Yaiba",
+        precio:" 50",
+        imagenes: [
+         "IMG/demonobani.jpe",
+         "IMG/demonobani 2.jpe",
+         "IMG/demonobani3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado.mide aprox 14cm"
+    },
+    {
+        nombre: "Figura Coleccionable Giyu Tomioka (Versión Sentada / Comiendo Onigiri) – Kimetsu no Yaiba",
+        precio:" 50",
+        imagenes: [
+         "IMG/demon agua.jpe",
+         "IMG/demon agua 2.jpe",
+         "IMG/demon agua 3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado.mide aprox 14cm"
+    },
+
+    {
+        nombre: "Figura Coleccionable Inosuke Hashibira (Máscara movible / Relax) – Kimetsu no Yaiba",
+        precio:" 50",
+        imagenes: [
+         "IMG/demonchancho.jpe",
+         "IMG/demonchanco 2.jpe",
+         "IMG/demonchancho 3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido.mide aprox 13cm"
+    },
+    {
+        nombre: "Figura Coleccionable Zenitsu Agatsuma (Versión Sentada / Comiendo Onigiri) – Kimetsu no Yaiba",
+        precio:" 50",
+        imagenes: [
+         "IMG/demon zeni.jpe",
+         "IMG/demon zeni 2.jpe",
+         "IMG/demon zeni 3.jpe"
+],
+        descripcion: "Material: PVC / ABS.mide aprox 14cm"
+    },
+
+     {
+        nombre: "Figura Coleccionable Gyomei Himejima – Pilar de la Roca",
+        precio:" 90",
+        imagenes: [
+         "IMG/demon reza.jpe",
+         "IMG/demon reza 2.jpe",
+         "IMG/demon reza 3.jpe"
+],
+        descripcion: "Material: PVC / ABS rígido.mide aprox 18cm"
+    },
+     {
+        nombre: "Figura Coleccionable Kyojuro Rengoku – Pilar de la Llama",
+        precio:" 90",
+        imagenes: [
+         "IMG/demon fuego.jpe",
+         "IMG/demosn fuego 2.jpe",
+         "IMG/demon fuego 3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS.mide aprox 17cm"
+    },
+    {
+        nombre: "Figura Coleccionable Gyutaro – Luna Superior 6 (Edición de Gran Escala)",
+        precio:" 170",
+        imagenes: [
+         "IMG/demonflaco.jpe",
+         "IMG/demonflaco 2.jpe",
+         "IMG/demonflaco 3.jpe"
+],
+        descripcion: "Material: PVC rígido y ABS de alta densidad.mide aprox 28cm"
+    },
+
+
+    {
+        nombre: "Figura Coleccionable Shanks El Pelirrojo (Versión Sentado en Cajón) – One Piece",
+        precio:" 200",
+        imagenes: [
+         "IMG/onepiecerojo.jpe",
+         "IMG/onepiece rojo 2.jpe",
+         "IMG/onepiece 3.jpe"
+],
+        descripcion: "mide aprox 16cm"
+    },
+    {
+        nombre: "Figura Coleccionable Silvers Rayleigh El Rey Oscuro (Versión Sentado) – One Piece",
+        precio:" 200",
+        imagenes: [
+         "IMG/FI1.jpe",
+         "IMG/fl12.jpe",
+         "IMG/fl13.jpe"
+],
+        descripcion: "mide aprox 16cm"
+    },
+     {
+        nombre: "Figura Coleccionable Meliodas – Pecado de la Ira del Dragón",
+        precio:" 70",
+        imagenes: [
+         "IMG/nanatsu.jpe",
+         "IMG/nanatsu 2.jpe",
+         "IMG/nanatsu 3.jpe"
+],
+        descripcion: "Material: PVC y ABS.mide aprox 17cm"
+    },
+     {
+        nombre: "Figura Coleccionable Denji & Pochita – Chainsaw Man (Fusión de Poder)",
+        precio:" 180",
+        imagenes: [
+         "IMG/chainsf.jpe",
+         "IMG/chainf 2.jpe",
+         "IMG/chainf 3.jpe"
+],
+        descripcion: "Material: PVC / ABS rígido con detalles de pintura sombreada.mide aprox 19cm"
+    },
+    {
+        nombre: "Figura Coleccionable Baki Hanma – Edición Limitada Grappler",
+        precio:" 100",
+        imagenes: [
+         "IMG/baki.jpe",
+         "IMG/baki 2.jpe",
+         "IMG/baki 3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS de alta densidad.Tamaño: Aprox. 16–18 cm."
+    },
+     {
+        nombre: "Figura Coleccionable The Joker – Versión Atraco al Banco (The Dark Knight)",
+        precio:" 80",
+        imagenes: [
+         "IMG/joker 2.jpe",
+         "IMG/joker 2.jpe",
+         "IMG/joker 3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado con acabados mate.mide aprox 16cm"
+    },
+     {
+        nombre: "Figura Coleccionable Sesshomaru (Versión Sentada) – Inuyasha",
+        precio:" 85",
+        imagenes: [
+         "IMG/seshomaru.jpe",
+         "IMG/seshomaru 2.jpe",
+         "IMG/seshomaru 3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado.mide aprox 13cm"
+    },
+    {
+        nombre: "Figura Chibi Sesshomaru con Espada – Edición Limitada Coleccionista",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibi shesho.jpe",
+         "IMG 2/chibi sesho2.jpe",
+         "IMG 2/chibi sesho3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido.mide aprox 10cm"
+    },
+    {
+        nombre: "Mini Figura Chibi Sesshomaru – Edición Mini Coleccionable",
+        precio:" 25",
+        imagenes: [
+         "IMG 2/chiki seshi chi.jpe",
+         "IMG 2/chiki seshi chi 2.jpe",
+         "IMG 2/chiki seshi 3.jpe"
+],
+        descripcion: "Material: PVC / ABS.mide aprox 8cm"
+    },
+    {
+        nombre: "Mini Figura Chibi Gaara Cosplay Shukaku (Una Cola) – Naruto",
+        precio:" 40",
+        imagenes: [
+         "IMG 2/chibi arena.jpe",
+         "IMG 2/chibi arena 2.jpe",
+         "IMG 2/chibi arena 3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado.mide aprox 8cm"
+    },
+    {
+        nombre: "Mini Figura Pakkun (Perro Ninja) Cosplay Kakashi Hatake – Naruto",
+        precio:" 40",
+        imagenes: [
+         "IMG 2/chibiperro.jpe",
+         "IMG 2/chibiperro 2.jpe",
+         "IMG 2/chibiperro 3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido.mide aprox 8cm"
+    },
+    {
+        nombre: "Mini Figura Chibi Kakashi Hatake con Kunai – Naruto Shippuden",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibenose.jpe",
+         "IMG 2/chibinose 2.jpe",
+         "IMG 2/chibenose.jpe"
+],
+        descripcion: "Material: PVC / ABS.mide aprox 9cm"
+    },
+    {
+        nombre: "Figura Chibi Zenitsu Agatsuma – Demon Slayer (Aventura Sin Límites)",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibizenit.jpe",
+         "IMG 2/chibizenit2.jpe",
+         "IMG 2/chibizenit3.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Chibi Chrollo Lucilfer – Líder de la Brigada Fantasma (Hunter x Hunter)",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chii hunter.jpe",
+         "IMG 2/chibi hunnter 2.jpe",
+         "IMG 2/chibi hunter 3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Chibi Denji – Cazador de Seguridad Pública (Chainsaw Man)",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibichains.jpe",
+         "IMG 2/chibichains2.jpe",
+         "IMG 2/chibichains3.jpe"
+],
+        descripcion: "Material: PVC rígido y plástico ABS.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Chibi Denji con Motosierra – Chainsaw Man",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibichainlengua.jpe",
+         "IMG 2/chibichainlengua2.jpe",
+         "IMG 2/chibichainlengua.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS.mide aprox 11cm"
+    },
+    {
+        nombre: "Figura Chibi Power con Espada de Sangre – Chainsaw Man",
+        precio:" 35",
+        imagenes: [
+         "IMG 2/chibimujer.jpe",
+         "IMG 2/chibimujer2.jpe",
+         "IMG 2/chibimujer3.jpe"
+],
+        descripcion: "Material: PVC / ABS moldeado."
+    },
+     {
+        nombre: "Figura Coleccionable  – Edición Akatsuki",
+        precio:" 25",
+        imagenes: [
+         "IMG 1/akat 1.jpe",
+         "IMG 1/akat 2.jpe",
+         "IMG 1/akat 3.jpe"
+],
+        descripcion: "Material: PVC rígido y ABS."
+    },
+     {
+        nombre: "Figura Coleccionable  – Edición Akatsuki",
+        precio:" 25",
+        imagenes: [
+         "IMG 1/akat 4.jpe",
+         "IMG 1/akat 5.jpe",
+         "IMG 1/akat 6.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido."
+    },
+     {
+        nombre: "Figura Coleccionable  – Edición Akatsuki",
+        precio:" 25",
+        imagenes: [
+         "IMG 1/akat 7.jpe",
+         "IMG 1/akat 8.jpe",
+         "IMG 1/akat 9.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido."
+    },
+     {
+        nombre: "Figura Coleccionable  – Edición Akatsuki",
+        precio:" 25",
+        imagenes: [
+         "IMG 1/akat 10.jpe",
+         "IMG 1/akat 11.jpe",
+         "IMG 1/akat 12.jpe"
+],
+        descripcion: "Material: PVC y plástico ABS rígido."
+    },
+
+
+   
+]
+
+const catalogo = document.getElementById("catalogo");
+const vermas = document.getElementById("vermas");
+const numerowhatsApp = "59178123121";
+
+let cantidadMostrar = 6;
+
+function mostrarproductos() {
+
+    catalogo.innerHTML = "";
+
+ for (let i = 0; i < cantidadMostrar && i <producto.length; i++) {
+
+        catalogo.innerHTML += `
+        <div class="col-4">
+        <div class="card">
+
+<div class="galeria">
+    <img src="${producto[i].imagenes[0]}" alt="${producto[i].nombre}">
+    <img src="${producto[i].imagenes[1]}" alt="${producto[i].nombre}">
+    <img src="${producto[i].imagenes[2]}" alt="${producto[i].nombre}">
+</div>
+         <div class="card-body">
+                        <h3 class="card-title">${producto[i].nombre}</h3>
+
+                        <p class="card-text">${producto[i].descripcion}</p>
+
+                        <h4>Bs. ${producto[i].precio}</h4>
+                        <a href="https://wa.me/${numerowhatsApp}?text=hola,%20quiero%20pedir%20la%20figura%20de${producto[i].nombre}.%20precio:%20bs.%20${producto[i].precio}"
+                           target="_blank"
+                         class="btn btn-primary">Pedir</a>
+                    </div>
+                </div>
+            </div>
+        `;
+ }
+
+} 
+mostrarproductos();
+
+vermas.addEventListener("click", function() {
+
+if (cantidadMostrar < producto.length) { 
+    cantidadMostrar += 6;
+    mostrarproductos();
+
+if (cantidadMostrar >= producto.length) {
+    vermas.textContent= "ver menos";
+
+}
+
+}else {
+    cantidadMostrar = 6;
+    mostrarproductos();
+    
+    vermas.textContent = "ver mas";
+}
+
+
+});
+    
+    
+    
